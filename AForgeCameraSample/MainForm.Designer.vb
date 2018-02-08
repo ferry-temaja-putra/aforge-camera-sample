7e8f5490b60da02b7f19923fdde4885b357f49c2
Ferry <ferry.temaja.putra@gmail.com>
2018-02-08 11:17:42 +0800

    Move from vs 2010.

--- AForgeCameraSample/MainForm.Designer.vb
+++ AForgeCameraSample/MainForm.Designer.vb
@@ -22,9 +22,194 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.recordingLbl = New System.Windows.Forms.Label()
+        Me.videoPnl = New System.Windows.Forms.Panel()
+        Me.videoPic = New System.Windows.Forms.PictureBox()
+        Me.Splitter1 = New System.Windows.Forms.Splitter()
+        Me.captureLsv = New System.Windows.Forms.ListView()
+        Me.captureImageCtx = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.RemoveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyPathToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.captureIml = New System.Windows.Forms.ImageList(Me.components)
+        Me.commandPnl = New System.Windows.Forms.Panel()
+        Me.resolutionCbo = New System.Windows.Forms.ComboBox()
+        Me.nextCameraBtn = New System.Windows.Forms.Button()
+        Me.recordBtn = New System.Windows.Forms.Button()
+        Me.captureBtn = New System.Windows.Forms.Button()
+        Me.elapsedTmr = New System.Windows.Forms.Timer(Me.components)
+        Me.videoPnl.SuspendLayout()
+        CType(Me.videoPic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.captureImageCtx.SuspendLayout()
+        Me.commandPnl.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'recordingLbl
+        '
+        Me.recordingLbl.AutoSize = True
+        Me.recordingLbl.BackColor = System.Drawing.Color.DarkRed
+        Me.recordingLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.recordingLbl.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.recordingLbl.Location = New System.Drawing.Point(13, 12)
+        Me.recordingLbl.Name = "recordingLbl"
+        Me.recordingLbl.Size = New System.Drawing.Size(106, 20)
+        Me.recordingLbl.TabIndex = 3
+        Me.recordingLbl.Text = "Recording..."
+        Me.recordingLbl.Visible = False
+        '
+        'videoPnl
+        '
+        Me.videoPnl.BackColor = System.Drawing.SystemColors.Window
+        Me.videoPnl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.videoPnl.Controls.Add(Me.recordingLbl)
+        Me.videoPnl.Controls.Add(Me.videoPic)
+        Me.videoPnl.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.videoPnl.Location = New System.Drawing.Point(200, 0)
+        Me.videoPnl.Name = "videoPnl"
+        Me.videoPnl.Padding = New System.Windows.Forms.Padding(10)
+        Me.videoPnl.Size = New System.Drawing.Size(584, 358)
+        Me.videoPnl.TabIndex = 7
+        '
+        'videoPic
+        '
+        Me.videoPic.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.videoPic.Location = New System.Drawing.Point(10, 10)
+        Me.videoPic.Name = "videoPic"
+        Me.videoPic.Size = New System.Drawing.Size(564, 338)
+        Me.videoPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.videoPic.TabIndex = 4
+        Me.videoPic.TabStop = False
+        '
+        'Splitter1
+        '
+        Me.Splitter1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Splitter1.Location = New System.Drawing.Point(200, 358)
+        Me.Splitter1.Name = "Splitter1"
+        Me.Splitter1.Size = New System.Drawing.Size(584, 3)
+        Me.Splitter1.TabIndex = 8
+        Me.Splitter1.TabStop = False
+        '
+        'captureLsv
+        '
+        Me.captureLsv.ContextMenuStrip = Me.captureImageCtx
+        Me.captureLsv.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.captureLsv.LargeImageList = Me.captureIml
+        Me.captureLsv.Location = New System.Drawing.Point(200, 361)
+        Me.captureLsv.Name = "captureLsv"
+        Me.captureLsv.Size = New System.Drawing.Size(584, 200)
+        Me.captureLsv.TabIndex = 5
+        Me.captureLsv.UseCompatibleStateImageBehavior = False
+        '
+        'captureImageCtx
+        '
+        Me.captureImageCtx.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RemoveToolStripMenuItem, Me.CopyPathToolStripMenuItem})
+        Me.captureImageCtx.Name = "captureImageCtx"
+        Me.captureImageCtx.Size = New System.Drawing.Size(130, 48)
+        '
+        'RemoveToolStripMenuItem
+        '
+        Me.RemoveToolStripMenuItem.Name = "RemoveToolStripMenuItem"
+        Me.RemoveToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
+        Me.RemoveToolStripMenuItem.Text = "Remove"
+        '
+        'CopyPathToolStripMenuItem
+        '
+        Me.CopyPathToolStripMenuItem.Name = "CopyPathToolStripMenuItem"
+        Me.CopyPathToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
+        Me.CopyPathToolStripMenuItem.Text = "Copy Path"
+        '
+        'captureIml
+        '
+        Me.captureIml.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.captureIml.ImageSize = New System.Drawing.Size(96, 96)
+        Me.captureIml.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'commandPnl
+        '
+        Me.commandPnl.BackColor = System.Drawing.SystemColors.Window
+        Me.commandPnl.Controls.Add(Me.resolutionCbo)
+        Me.commandPnl.Controls.Add(Me.nextCameraBtn)
+        Me.commandPnl.Controls.Add(Me.recordBtn)
+        Me.commandPnl.Controls.Add(Me.captureBtn)
+        Me.commandPnl.Dock = System.Windows.Forms.DockStyle.Left
+        Me.commandPnl.Location = New System.Drawing.Point(0, 0)
+        Me.commandPnl.Name = "commandPnl"
+        Me.commandPnl.Size = New System.Drawing.Size(200, 561)
+        Me.commandPnl.TabIndex = 6
+        '
+        'resolutionCbo
+        '
+        Me.resolutionCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.resolutionCbo.FormattingEnabled = True
+        Me.resolutionCbo.Location = New System.Drawing.Point(11, 150)
+        Me.resolutionCbo.Name = "resolutionCbo"
+        Me.resolutionCbo.Size = New System.Drawing.Size(171, 28)
+        Me.resolutionCbo.TabIndex = 3
+        '
+        'nextCameraBtn
+        '
+        Me.nextCameraBtn.Location = New System.Drawing.Point(11, 104)
+        Me.nextCameraBtn.Name = "nextCameraBtn"
+        Me.nextCameraBtn.Size = New System.Drawing.Size(171, 40)
+        Me.nextCameraBtn.TabIndex = 2
+        Me.nextCameraBtn.Text = "Next Camera"
+        Me.nextCameraBtn.UseVisualStyleBackColor = True
+        '
+        'recordBtn
+        '
+        Me.recordBtn.Location = New System.Drawing.Point(11, 58)
+        Me.recordBtn.Name = "recordBtn"
+        Me.recordBtn.Size = New System.Drawing.Size(171, 40)
+        Me.recordBtn.TabIndex = 1
+        Me.recordBtn.Text = "Record"
+        Me.recordBtn.UseVisualStyleBackColor = True
+        '
+        'captureBtn
+        '
+        Me.captureBtn.Location = New System.Drawing.Point(11, 12)
+        Me.captureBtn.Name = "captureBtn"
+        Me.captureBtn.Size = New System.Drawing.Size(171, 40)
+        Me.captureBtn.TabIndex = 0
+        Me.captureBtn.Text = "Capture"
+        Me.captureBtn.UseVisualStyleBackColor = True
+        '
+        'elapsedTmr
+        '
+        Me.elapsedTmr.Interval = 1000
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.videoPnl)
+        Me.Controls.Add(Me.Splitter1)
+        Me.Controls.Add(Me.captureLsv)
+        Me.Controls.Add(Me.commandPnl)
+        Me.Name = "MainForm"
         Me.Text = "Form1"
+        Me.videoPnl.ResumeLayout(False)
+        Me.videoPnl.PerformLayout()
+        CType(Me.videoPic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.captureImageCtx.ResumeLayout(False)
+        Me.commandPnl.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents recordingLbl As Label
+    Friend WithEvents videoPnl As Panel
+    Friend WithEvents videoPic As PictureBox
+    Friend WithEvents Splitter1 As Splitter
+    Friend WithEvents captureLsv As ListView
+    Friend WithEvents captureImageCtx As ContextMenuStrip
+    Friend WithEvents RemoveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyPathToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents captureIml As ImageList
+    Friend WithEvents commandPnl As Panel
+    Friend WithEvents resolutionCbo As ComboBox
+    Friend WithEvents nextCameraBtn As Button
+    Friend WithEvents recordBtn As Button
+    Friend WithEvents captureBtn As Button
+    Friend WithEvents elapsedTmr As Timer
 End Class
--- AForgeCameraSample/MainForm.vb
+++ AForgeCameraSample/MainForm.vb
@@ -1,3 +1,282 @@
-﻿Public Class MainForm
+﻿Imports System.IO
+Imports System.Threading
+Imports AForge.Video.DirectShow
+Imports AForge.Video.FFMPEG
+
+Public Class MainForm
+
+    Private videoDevices As FilterInfoCollection
+    Private videoSource As VideoCaptureDevice
+    Private currentVideoDeviceIndex As Integer
+    Private videoWriter As VideoFileWriter
+    Private stopwatch As Stopwatch
+
+    Private isStoppingRecording As Boolean
+    Private isRecording As Boolean
+    Private isCapturing As Boolean
+    Private tempVideoKey As String
+    Private videoWriterLock As Object
+
+    Sub New()
+
+        InitializeComponent()
+
+        videoDevices = New FilterInfoCollection(FilterCategory.VideoInputDevice)
+        videoWriter = New VideoFileWriter
+        stopwatch = New Stopwatch
+
+        If videoDevices.Count > 0 Then
+            currentVideoDeviceIndex = 0
+            ConfigureVideoSource()
+        End If
+
+        isCapturing = False
+        isRecording = False
+        isStoppingRecording = False
+        videoWriterLock = New Object
+
+    End Sub
+
+    Private Sub ConfigureVideoSource()
+
+        Dim isStartRequired = False
+
+        If videoSource IsNot Nothing AndAlso videoSource.IsRunning Then
+            ReleaseVideoSource()
+            isStartRequired = True
+        End If
+
+        videoSource = New VideoCaptureDevice(videoDevices(currentVideoDeviceIndex).MonikerString)
+        AddHandler videoSource.NewFrame, AddressOf videoSource_NewFrame
+
+        PopulateResolutionControl()
+        SetVideoResolution(videoSource.VideoCapabilities.First)
+
+        If isStartRequired Then
+            videoSource.Start()
+        End If
+    End Sub
+
+    Private Sub ReleaseVideoSource()
+        videoSource.SignalToStop()
+        videoSource.WaitForStop()
+        RemoveHandler videoSource.NewFrame, AddressOf videoSource_NewFrame
+    End Sub
+
+    Private Sub PopulateResolutionControl()
+
+        resolutionCbo.Items.Clear()
+
+        For Each capability In videoSource.VideoCapabilities
+            resolutionCbo.Items.Add(String.Format("{0} x {1} - {2} FPS", capability.FrameSize.Width, capability.FrameSize.Height, capability.AverageFrameRate))
+        Next
+
+        resolutionCbo.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub SetVideoResolution(capability As VideoCapabilities)
+
+        Dim isStartRequired = False
+
+        If videoSource IsNot Nothing AndAlso videoSource.IsRunning Then
+            videoSource.SignalToStop()
+            videoSource.WaitForStop()
+            isStartRequired = True
+        End If
+
+        videoSource.VideoResolution = capability
+
+        If isStartRequired Then
+            videoSource.Start()
+        End If
+
+    End Sub
+
+    Private Sub StartRecording()
+        tempVideoKey = Guid.NewGuid.ToString
+        videoWriter.Open(Path.Combine(Path.GetTempPath, tempVideoKey & ".avi"),
+                         videoSource.VideoResolution.FrameSize.Width,
+                         videoSource.VideoResolution.FrameSize.Height,
+                         videoSource.VideoResolution.AverageFrameRate,
+                         VideoCodec.MPEG4)
+    End Sub
+
+    Private Sub StopRecording(image As Image)
+        videoWriter.Close()
+
+        Dim listViewItem As New ListViewItem
+        With listViewItem
+            .Name = tempVideoKey
+            .Text = tempVideoKey
+            .ImageKey = tempVideoKey
+            .Tag = "avi"
+        End With
+
+        Me.BeginInvoke(Sub()
+                           captureIml.Images.Add(tempVideoKey, image)
+                           captureLsv.Items.Add(listViewItem)
+                       End Sub)
+
+    End Sub
+
+    Private Sub WriteVideo(image As Object)
+        SyncLock videoWriterLock
+            videoWriter.WriteVideoFrame(DirectCast(image, Bitmap))
+        End SyncLock
+    End Sub
+
+    Private Sub CaptureImage(image As Image)
+
+        Dim imageKey = Guid.NewGuid().ToString
+
+        image.Save(Path.Combine(Path.GetTempPath, imageKey & ".bmp"))
+
+        Dim listViewItem As New ListViewItem
+        With listViewItem
+            .Name = imageKey
+            .Text = imageKey
+            .ImageKey = imageKey
+            .Tag = "bmp"
+        End With
+
+        BeginInvoke(Sub()
+                        captureIml.Images.Add(imageKey, image)
+                        captureLsv.Items.Add(listViewItem)
+                    End Sub)
+    End Sub
+
+    Private Sub DisplayElapsedLabel()
+        recordingLbl.Text = String.Format("Recording...[{0:00}:{1:00}:{2:00}]",
+                                          stopwatch.Elapsed.Hours,
+                                          stopwatch.Elapsed.Minutes,
+                                          stopwatch.Elapsed.Seconds)
+    End Sub
+
+    Private Function GetFileName(listViewItem As ListViewItem) As String
+        Dim fileName = String.Format("{0}.{1}", listViewItem.ImageKey, listViewItem.Tag)
+        Return Path.Combine(Path.GetTempPath, fileName)
+    End Function
+
+    Private Sub videoSource_NewFrame(sender As Object, eventArgs As AForge.Video.NewFrameEventArgs)
+        Dim frame = New Bitmap(eventArgs.Frame)
+
+        If isCapturing Then
+            isCapturing = False
+            CaptureImage(New Bitmap(frame))
+        End If
+
+        If isRecording Then
+            ThreadPool.QueueUserWorkItem(AddressOf WriteVideo, New Bitmap(frame))
+        End If
+
+        If isStoppingRecording Then
+            isStoppingRecording = False
+            StopRecording(New Bitmap(frame))
+        End If
+
+        If videoPic.Image IsNot Nothing Then
+            videoPic.Image.Dispose()
+        End If
+
+        videoPic.Image = frame
+    End Sub
+
+    Private Sub captureBtn_Click(sender As System.Object, e As System.EventArgs) Handles captureBtn.Click
+        isCapturing = True
+    End Sub
+
+    Private Sub recordBtn_Click(sender As System.Object, e As System.EventArgs) Handles recordBtn.Click
+
+        If isRecording Then
+            isRecording = False
+            isStoppingRecording = True
+            stopwatch.Stop()
+            elapsedTmr.Stop()
+            recordingLbl.Hide()
+            recordBtn.Text = "Record"
+        Else
+            isRecording = True
+            stopwatch.Reset()
+            stopwatch.Start()
+            elapsedTmr.Start()
+            recordingLbl.Show()
+            DisplayElapsedLabel()
+            recordBtn.Text = "Stop"
+            StartRecording()
+        End If
+
+    End Sub
+
+    Private Sub resolutionCbo_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles resolutionCbo.SelectedIndexChanged
+        SetVideoResolution(videoSource.VideoCapabilities(resolutionCbo.SelectedIndex))
+    End Sub
+
+    Private Sub nextCameraBtn_Click(sender As System.Object, e As System.EventArgs) Handles nextCameraBtn.Click
+
+        If isRecording Then
+            MessageBox.Show("Please stop the recording before switching the camera!", "Cannot Proceed", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        currentVideoDeviceIndex += 1
+
+        If currentVideoDeviceIndex > videoDevices.Count - 1 Then
+            currentVideoDeviceIndex = 0
+        End If
+
+        ConfigureVideoSource()
+    End Sub
+
+    Private Sub captureLsv_DoubleClick(sender As System.Object, e As System.EventArgs) Handles captureLsv.DoubleClick
+
+        If captureLsv.SelectedItems.Count = 0 Then Return
+
+        Dim fileName = GetFileName(captureLsv.SelectedItems(0))
+
+        Process.Start(fileName)
+
+    End Sub
+
+    Private Sub elapsedTmr_Tick(sender As System.Object, e As System.EventArgs) Handles elapsedTmr.Tick
+        DisplayElapsedLabel()
+    End Sub
+
+    Private Sub RemoveToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles RemoveToolStripMenuItem.Click
+
+        If captureLsv.SelectedItems.Count = 0 Then Return
+
+        Dim imageKey = captureLsv.SelectedItems(0).ImageKey
+
+        captureLsv.Items.RemoveByKey(imageKey)
+        captureIml.Images.RemoveByKey(imageKey)
+
+    End Sub
+
+    Private Sub CopyPathToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles CopyPathToolStripMenuItem.Click
+        If captureLsv.SelectedItems.Count = 0 Then Return
+
+        Dim fileName = GetFileName(captureLsv.SelectedItems(0))
+
+        Clipboard.SetText(fileName)
+
+        MessageBox.Show("File Name is copied to the clipboard", "File Name Copied", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub MainForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        videoSource.Start()
+    End Sub
+
+    Private Sub MainForm_FormClosing(sender As System.Object, e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+
+        If isRecording Then
+            isStoppingRecording = True
+        End If
+
+        If videoSource IsNot Nothing AndAlso videoSource.IsRunning Then
+            ReleaseVideoSource()
+        End If
+
+    End Sub
 
 End Class
